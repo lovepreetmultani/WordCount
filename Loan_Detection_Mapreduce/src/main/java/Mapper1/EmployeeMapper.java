6e44f2512221a9aadd2d6df2f172b0f556f032ee
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-03-09 10:08:39 +0000

    Initial commit

--- Loan_Detection_Mapreduce/src/main/java/Mapper1/EmployeeMapper.java
+++ Loan_Detection_Mapreduce/src/main/java/Mapper1/EmployeeMapper.java
@@ -1,4 +1,10 @@
 package Mapper1;
 
-public class EmployeeMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class EmployeeMapper extends Mapper<Text, LongWritable, Text, LongWritable> {
+
+
 }
